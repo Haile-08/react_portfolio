f2b71a342510b671b867695f1ba9aa7a97a67468
Haile-08 <haile4cmd@gmail.com>
2023-08-21 17:21:21 +0300

    Add the humburger menu

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,5 +1,6 @@
 import { Dispatch, SetStateAction } from "react";
 import "./Navigation.css";
+import MenuButton from "../Home/MenuButton/MenuButton";
 
 type navProps = {
   open?: boolean;
@@ -7,7 +8,16 @@ type navProps = {
 };
 
 function Navigation({ open, setOpen }: navProps) {
-  return <div className="navigation">Navigation</div>;
+  return (
+    <div className="navigation">
+      <div className="navigation-nav">
+        <div className="navigation-hamburger-menu">
+          <MenuButton open={open} />
+        </div>
+      </div>
+      <div className="navigation-body"></div>
+    </div>
+  );
 }
 
 export default Navigation;
