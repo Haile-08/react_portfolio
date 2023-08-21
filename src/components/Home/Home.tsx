840f418abf85fd6e26d72c3bf69a44886520b501
Haile-08 <haile4cmd@gmail.com>
2023-08-21 12:11:47 +0300

    Add new font lato

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,7 +2,21 @@ import React from "react";
 import "./Home.css";
 
 function Home() {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <div className="home-nav">
+        <div className="home-hamburger-menu">x</div>
+      </div>
+      <div className="home-body">
+        <div className="home-body-info">
+          <p className="name-info">Haile Melaku</p>
+          <p className="career-info">Software engineer, Designer</p>
+          <p className="decor-info">Hello</p>
+          <p className="decor-info">world</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
