669021ca90af742653f11c172d4dc5a35c813ca5
Haile-08 <haile4cmd@gmail.com>
2023-08-25 11:35:37 +0300

    Add a demo page for the two projects

---
+++ src/components/Demo1/Demo1.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Demo1.css";
+
+function Demo1() {
+  return <div>Demo1</div>;
+}
+
+export default Demo1;
---
+++ src/components/Demo2/Demo2.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Demo2.css";
+
+function Demo2() {
+  return <div>Demo2</div>;
+}
+
+export default Demo2;
