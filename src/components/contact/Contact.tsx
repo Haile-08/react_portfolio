da8491d5762785f1f88ae0ef66f1f7c36d799413
Haile-08 <haile4cmd@gmail.com>
2023-09-04 14:31:12 +0300

    Create a user interface for the contact from

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import "./Contact.css";
+import { motion } from "framer-motion";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="contact">
+      <div className="contact-title">
+        <p>Contact me</p>
+      </div>
+      <div className="contact-body">
+        <p>SAY HI</p>
+        <form action="">
+          <div className="contact-top">
+            <input
+              type="email"
+              name="email"
+              className="contact-email"
+              placeholder="Email"
+            />
+            <input
+              type="text"
+              name="subject"
+              className="contact-subject"
+              placeholder="Subject"
+            />
+          </div>
+          <textarea
+            name="body"
+            className="contact-input-body"
+            placeholder="Body"
+          />
+          <motion.button
+            type="submit"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.9 }}
+            transition={{ type: "spring", stiffness: 400, damping: 17 }}
+          >
+            Send
+          </motion.button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
