3399d0908aa979e5f63990e853a19b70f71bbc4c
Haile-08 <haile4cmd@gmail.com>
2023-09-05 11:51:50 +0300

    Add form validation for contact me
    the form validation is made with react form hook
    and validbot resolver

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,7 +1,40 @@
-import "./Contact.css";
 import { motion } from "framer-motion";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { valibotResolver } from "@hookform/resolvers/valibot";
+import { object, string, minLength, email } from "valibot";
+import error from "../../assets/error.png";
+import "./Contact.css";
+
+interface IFormInputs {
+  email: string;
+  subject: string;
+  body: string;
+}
+
+const schema = object({
+  email: string("Your email must be a string.", [
+    minLength(1, "Please enter your email."),
+    email("The email address is badly formatted."),
+  ]),
+  subject: string("email subject is required", [
+    minLength(3, "Needs to be at least 3 characters"),
+  ]),
+  body: string("email body is required", [
+    minLength(3, "Needs to be at least 3 characters"),
+  ]),
+});
+
+const onSubmit: SubmitHandler<IFormInputs> = (data) => console.log(data);
 
 function Contact() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInputs>({
+    resolver: valibotResolver(schema),
+  });
+
   return (
     <div className="contact">
       <div className="contact-title">
@@ -9,26 +42,56 @@ function Contact() {
       </div>
       <div className="contact-body">
         <p>SAY HI</p>
-        <form action="">
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="contact-top">
-            <input
-              type="email"
-              name="email"
-              className="contact-email"
-              placeholder="Email"
-            />
-            <input
-              type="text"
-              name="subject"
-              className="contact-subject"
-              placeholder="Subject"
-            />
+            <div className="input-box">
+              <input
+                className={
+                  "contact-email-" + (errors?.email ? "show" : "hidden")
+                }
+                placeholder="Email"
+                {...register("email")}
+              />
+              {errors?.email && (
+                <p className="error">
+                  <img src={error} alt="error" />
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+            <div className="input-box">
+              <input
+                className={
+                  "contact-subject-" + (errors?.subject ? "show" : "hidden")
+                }
+                placeholder="Subject"
+                {...register("subject")}
+              />
+              {errors.subject && (
+                <p className="error">
+                  <img src={error} alt="error" />
+                  {errors.subject.message}
+                </p>
+              )}
+            </div>
           </div>
-          <textarea
-            name="body"
-            className="contact-input-body"
-            placeholder="Body"
-          />
+          <div className="input-box">
+            <textarea
+              className={
+                "contact-input-body-" + (errors?.body ? "show" : "hidden")
+              }
+              placeholder="Body"
+              {...register("body")}
+            />
+            {errors.body && (
+              <p className="error-text">
+                {" "}
+                <img src={error} alt="error" />
+                {errors.body.message}
+              </p>
+            )}
+          </div>
+
           <motion.button
             type="submit"
             whileHover={{ scale: 1.05 }}
