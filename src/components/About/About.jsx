1ba5e35b07e826f91f3bf746afac2b27ecba405b
Haile-08 <haile4cmd@gmail.com>
2023-09-11 15:16:55 +0300

    Add info to the about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,12 +10,35 @@ function About() {
         </div>
         <div className="about-info">
           <div className="about-bar">
-            <p>
-              Hi, I specialize in developing cutting-edge applications that
-              drive business growth and leave a lasting impression. With a keen
-              eye for exceptional design and a passion for seamless
-              functionality, I bring your ideas to life.
-            </p>
+            <div className="about-description">
+              <h1>Description</h1>
+              <hr />
+              <p>
+                HI, I am a highly skilled and dedicated Software Developer with
+                a Bachelor's degree in Computer Science and Engineering,
+                complemented by a one-year intensive Software engineering boot
+                camp from Holberton School. I bring over three years of
+                professional experience in the field, consistently delivering
+                polished, linted, formatted, and thoroughly tested projects.
+              </p>
+            </div>
+            <div className="about-education">
+              <h1>EDUCATION</h1>
+              <hr />
+              <div className="edu1">
+                <p>02.2019- 10.2023</p>
+                <p>
+                  Adama Science And Technology University - Computer science and
+                  engineering (CSE)
+                </p>
+              </div>
+              <hr />
+              <div className="edu2">
+                <p>05.2022 - 01.2024</p>
+                <p>Holberton School - Software engineering</p>
+              </div>
+            </div>
+
             <button>Resume</button>
           </div>
         </div>
